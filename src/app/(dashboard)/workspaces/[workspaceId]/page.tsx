908d043022e58ba4f649b2dec8b82912103bed9b
laurhakob <laurhakob@gmail.com>
2024-12-13 19:10:22 +0400

    invite system

--- src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -4,7 +4,7 @@ import { redirect } from "next/navigation";
 const WorkspaceIdPage = async () => {
   const user = await getCurrent();
   if (!user) redirect("/sign-in");
-  return <div>Workspace Id</div>;
+  return <div>Workspace Id </div>;
 };
 
 export default WorkspaceIdPage;
